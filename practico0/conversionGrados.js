dca0d2ed21fef14e467508f0442c62237058cfbb
Fede <fedemmenendez@gmail.com>
2023-05-27 15:34:21 -0300

    Agrego el archivo conversionGrados.js

--- practico0/conversionGrados.js
+++ practico0/conversionGrados.js
@@ -3,5 +3,5 @@ let grados_fahrenheit1=75;
 let grados_celsius= (grados_fahrenheit-32) * 5/9;
 let grados_celsius1= (grados_fahrenheit1-32)*5/9;
 console.log ("50°F son " + grados_celsius + " °C");
-console.log (+ grados_celsius1 + " °C");
+console.log ("75°F son "+ grados_celsius1 + " °C");
 // 50°F son 10°C y 75°F son 23,89°C
