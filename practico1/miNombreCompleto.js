3cefa901a118b3ccb4549e3fbfc04205f7bcabae
Fede <fedemmenendez@gmail.com>
2023-06-09 17:18:24 -0300

    subo archivo ejercicio 1

--- practico1/miNombreCompleto.js
+++ practico1/miNombreCompleto.js
@@ -2,5 +2,4 @@ let nombre;
 nombre = "Federico";
 let apellido = "Menendez";
 let edad = 32;
-console.log ("Hola, "+ nombre + " " + apellido +". Tienes " 
-+ edad  + " años.");
+console.log ("Hola, "+ nombre + " " + apellido +". Tienes " + edad  + " años.");
