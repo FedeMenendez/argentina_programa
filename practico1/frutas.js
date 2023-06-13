e185da31a42f5ac8d9e3b37fd74471ca4d57f677
Fede <fedemmenendez@gmail.com>
2023-06-12 21:44:02 -0300

    Subo el archivo frutas.js

---
+++ practico1/frutas.js
@@ -0,0 +1,6 @@
+let frutas = [];
+frutas.push ("Manzana","Pera","Sandia","Mandarina","Naranja")
+let ultima = frutas [frutas.length-1];
+let primera = frutas [0];
+console.log ("La ultima fruta agregada fue: "+ ultima);
+console.log ("La primera fruta agregada fue: " + primera);
